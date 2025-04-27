884de43a270fa4f953e4321e608da9113b6df5b6
Matthew Duarte <hello.codehipster@outlook.com>
2025-04-27 13:58:22 -0500

    holamundo.php

---
+++ Holamundo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+print "Hola mundo php con print <br>"; //Nos permite poner un solo mensaje
+echo "Hola mundo PHP con echo", "Varios mensajes"; // Nos permite poner varios mensajes
+?>
+    
+</body>
+</html>
